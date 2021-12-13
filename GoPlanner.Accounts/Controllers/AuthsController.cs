9d71f00e4f75067b992ddfc53e08e1ff02ac8706
Abdullahi Yusuf <abdul.benyusuf@gmail.com>
2021-12-13 23:44:53 +0100

    initial

---
+++ GoPlanner.Accounts/Controllers/AuthsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace GoPlanner.Accounts.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthsController : ControllerBase
+    {
+        private readonly ILogger<AuthsController> _logger;
+
+        public AuthsController(ILogger<AuthsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [SwaggerResponse(StatusCodes.Status200OK, type: typeof(bool), description: "Login successful")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, type: typeof(bool), description: "Invalid login credentials")]
+        public IActionResult Authenticate(string username, string password)
+        {
+            _logger.LogInformation("Starting to authenticate");
+
+            var isAuthenticated = username == "admin" && password == "pass";
+
+            return isAuthenticated ? Ok(true) : Unauthorized(false);
+        }
+    }
+}
---
+++ GoPlanner.Accounts/Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace GoPlanner.Accounts
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.AddServer(new OpenApiServer
+                {
+                    Url = "https://localhost:5001",
+                    Description = "Localhost"
+                });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Accounts",
+                    Version = "1.0",
+                    Description = "Accounts API",
+                    Contact = new OpenApiContact
+                    {
+                        Email = "hello@techbay.ng",
+                        Name = "Techbay Solutions Limited",
+                        Url = new Uri("https://techbay.ng")
+                    }
+                });
+                c.IncludeXmlComments(Path.ChangeExtension(Assembly.GetEntryAssembly().Location, "xml"));
+                c.CustomSchemaIds(x => x.FullName);
+
+            });
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                var swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
+                c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json",
+                    $"Accounts API API 1.0");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
